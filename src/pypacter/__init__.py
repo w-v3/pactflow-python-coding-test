41438c6ebfc72dd2f79a52a0aa2a43e044752e68
JP-Ellis <josh@jpellis.me>
2024-02-22 15:56:56 +1100

    feat(core)!: add Python configuration
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ src/pypacter/__init__.py
@@ -0,0 +1,23 @@
+"""
+PyPacter Core Logic.
+
+This package contains the core logic for the PyPacter application.
+"""
+
+from pypacter.__version__ import __version__, __version_tuple__
+
+__author__ = "Joshua Ellis"
+__email__ = "joshua.ellis@smartbear.com"
+__url__ = "https://github.com/pactflow/pactflow-python-coding-test"
+__license__ = "Proprietary"
+__copyright__ = "SmartBear Software and PactFlow"
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "__author__",
+    "__email__",
+    "__url__",
+    "__license__",
+    "__copyright__",
+]
